a5e02bb4c08b1250d00217d825751f84141569c3
Julian Castro <julian_castro1@icloud.com>
2023-03-13 09:11:45 -0500

    added inital logic

--- test.js
+++ test.js
@@ -1,3 +1,39 @@
-var outField = document.querySelector('#t');
+document.addEventListener("DOMContentLoaded", function () {
+  var outField = document.getElementById("out");
+  var but = document.querySelector("button");
+  but.addEventListener("click", onclick)
+});
+function onclick(){
+    playRound();
+}
+function won(str){
+    console.log(name + " mightve won");
+    var name = document.getElementById('player');
+    switch(str){
+        case 't': outField.textContent = "Tie!";
+        case 'p': outField.textContent = name + " wins!";
+        case 'c': outField.textContent = "Computer wins!";
+    }
+}
+function playRound(playerSelect){
+    var compOptions = ['paper','rock','scissors'];
+    var perChoice = document.getElementById('selection');
+    var compChoice = compOptions[Math.floor(Math.random() * compOptions.length)];
 
-outField.textContent = "Hello";
+    if(perChoice == compChoice){
+        won('t');
+    } else {
+        // determine winner
+        switch (perChoice) {
+          case "paper":
+            compChoice == "rock" ? won("p") : won("c");
+            break;
+          case "rock":
+            compChoice == "scissors" ? won("p") : won("c");
+            break;
+          case "scissors":
+            compChoice == "rock" ? won("c") : won("p");
+            break;
+        }
+    }
+}
