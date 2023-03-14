f96f3cd9f93b31268fe33ecb5b44afa85ec6e955
Julian Castro <julian_castro1@icloud.com>
2023-03-14 08:09:38 -0500

    new GUI

--- test.js
+++ test.js
@@ -1,13 +1,72 @@
+let rBut, pBut, sBut, goBut, playerName, playScore, compScore, compOut, curButSelect;
+
 document.addEventListener("DOMContentLoaded", function () {
-  var outField = document.getElementById("out");
-  var but = document.querySelector("button");
-  but.addEventListener("click", onclick)
-  var inputs = document.querySelectorAll('input');
+  // statuses
+  curButSelect = null;
+
+  // buttons
+  rBut = document.getElementById("rock");
+  pBut = document.getElementById("paper");
+  sBut = document.getElementById("scissors");
+  goBut = document.getElementById("go");
+
+  // fields
+  playerName = document.getElementById("pName");
+  playScore = document.getElementById("playerScore");
+  compScore = document.getElementById("compScore");
+  compOut = document.getElementById("compRes");
+
+  // event listeners
+  rBut.addEventListener("click", selectBut);
+  pBut.addEventListener("click", selectBut);
+  sBut.addEventListener("click", selectBut);
+
+  goBut.addEventListener("click", go);
+
+  playerName.addEventListener("click", changeName);
 });
+
+
+
 function onclick(){
     playRound();
 }
+function selectBut(){
+  if(curButSelect != null){ curButSelect.style.backgroundColor = "#b8b8b8";}
+  this.style.backgroundColor = "#a2ba9f";
+  curButSelect = this;
+}
+function go(){
+  if(curButSelect == null){ alert("no option selected"); return; }
+  curButSelect.style.backgroundColor = "#b8b8b8";
+  let choice = curButSelect.name;
+  curButSelect = null;
 
+  let winner = playRound(choice);
+  if(winner == 't'){
+    document.getElementById("screenLeft").style.backgroundColor = "#a5dfb1";
+    document.getElementById("screenRight").style.backgroundColor = "#a5dfb1";
+    setTimeout(function () {
+      document.getElementById("screenLeft").style.backgroundColor = "#d9d9d9";
+      document.getElementById("screenRight").style.backgroundColor = "#d9d9d9";
+    }, 1000);
+  } else if(winner == 'c'){
+    document.getElementById("screenRight").style.backgroundColor = "#a5dfb1";
+    compScore.innerHTML++;
+    setTimeout(function () {
+      document.getElementById("screenRight").style.backgroundColor = "#d9d9d9";
+    }, 1000);
+  } else {
+    document.getElementById("screenLeft").style.backgroundColor = "#a5dfb1";
+    playScore.innerHTML++;
+    setTimeout(function () {
+      document.getElementById("screenLeft").style.backgroundColor = "#d9d9d9";
+    }, 1000);
+  }
+}
+function changeName(){
+
+}
 function won(str){
     var outField = document.getElementById("out");
     console.log(name + " mightve won");
@@ -20,24 +79,28 @@ function won(str){
 }
 function playRound(playerSelect){
     var compOptions = ['paper','rock','scissors'];
-    var perChoice = document.getElementById('selection').value;
+    var perChoice = playerSelect;
     var compChoice = compOptions[Math.floor(Math.random() * compOptions.length)];
+    compOut.innerHTML = compChoice;
+    setTimeout(function () {
+      compOut.innerHTML = "???";
+    }, 2000);
 
     if(perChoice == compChoice){
-        won('t');
+        return "t";
     } else {
         // determine winner
         switch (perChoice) {
           case "paper":
-            compChoice == "rock" ? won("p") : won("c");
+            return compChoice == "rock" ? "p" : "c";
             break;
           case "rock":
-            compChoice == "scissors" ? won("p") : won("c");
+            return compChoice == "scissors" ? "p" : "c";
             break;
           case "scissors":
-            compChoice == "rock" ? won("c") : won("p");
+            return compChoice == "rock" ? "c" : "p";
             break;
-          default: document.getElementById("out").textContent = "Invalid Selection!"
+          default: alert("Invalid Selection!");
         }
     }
 }
